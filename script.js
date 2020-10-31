d7f44181af36dd41e7eec1e5570e428e6866d7a5
jelolo <jelolo@gmail.com>
2020-10-31 02:00:10 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 
-function msg() {
-    var name = prompt('Input Your Name');
-    var wish = prompt('write her a birthday message')
-    alert('Thank You '+ name)
-    document.getElementById('nameinput').innerHTML = name + ":"  + " "  + " " + wish
+// function msg() {
+//     var name = prompt('Input Your Name');
+//     var wish = prompt('write her a birthday message')
+//     alert('Thank You '+ name)
+//     document.getElementById('nameinput').innerHTML = name + ":"  + " "  + " " + wish
     
-}
+// }
+
+document.addEventListener('DOMContentLoaded', () =>{
+    document.querySelector('#new_task').onsubmit = () =>{
+        const li = document.createElement('li');
+        li.innerHTML = document.querySelector('#task').value + ":"  + " " + document.querySelector('#taskmessage').value
+        document.querySelector('#tasks').append(li);
+        document.querySelector('#task').value = '';
+        document.querySelector('#taskmessage').value = '';
+        return false;
+    };
+})
